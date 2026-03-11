ff1b19a82730eba40342b6403f04a8120f2ad64c
Tharmi1305 <tharmihansenthur01@gmail.com>
2026-03-11 17:29:45 +0530

    Convert GameScreen to StatefulWidget for game state management

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
 
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
